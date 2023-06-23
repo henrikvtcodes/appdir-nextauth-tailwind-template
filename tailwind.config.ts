41933b591be0e717294efe15ac4c14ebb9909532
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-06-23 17:19:17 -0400

    inter font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
